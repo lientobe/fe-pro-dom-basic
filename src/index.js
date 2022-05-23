e07f086f7f4f9b75d76501c17fe5ff451662c7e7
Anzhelika <lientobe@gmail.com>
2022-05-23 13:50:57 +0300

    fixed hw

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
-export const paintCards = () => {};
+export const paintCards = () => {
+  const lis = document.querySelectorAll('.item:nth-child(odd)');
+  for (let li of lis) {
+    li.style.backgroundColor = 'red';
+  }
+};
 
-export const findElement = () => {};
+export const findElement = () => {
+  let li = document.querySelector('.item');
+
+  while (li) {
+    if (li.matches('.likedItem')) {
+      li.style.backgroundColor = 'blue';
+    }
+    li = li.nextElementSibling;
+  }
+};
